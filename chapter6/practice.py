1ce6ee1191ffb275434f135ebfb5c1935d5a1b3a
ruzhan05 <ruzhan05@gmail.com>
2025-05-03 12:11:37 +0200

    conditionals

---
+++ chapter6/practice.py
@@ -0,0 +1,19 @@
+# name_list = ["Ruzhan" , "Aiman", "Ammu", "Abbu"]
+# name = input("Enter the name : ")
+# if(name == name_list[0]):
+#     print("This is me")
+# elif(name in name_list):
+#     print("This is family!")
+# else:
+#     print("Not family or a string")
+    
+
+
+
+###another code
+
+post = input("Enter the post")
+if ("Harry" in post):
+    print("the post contains the word \"Harry\"")
+else:
+    print("The post does not contain the word\"Harry\"")
